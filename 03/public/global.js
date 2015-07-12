c47ed3e8967515004479e173cfd371c2a493b2f3
Kristin Houchin <kmhouchin@gmail.com>
2015-07-12 18:39:24 -0500

    finish 3 now understanding the functions of Ajax. Many uses.

--- 03/public/global.js
+++ 03/public/global.js
@@ -1,19 +1,21 @@
-var req = new XMLHttpRequest();
 
-req.open("get", "/path1");
-
-req.addEventListener("load", function(){
-  alert(this.response);
-});
-
-req.send();
 
 //  ---------------------------------------------------------------------------
 
 var myLink = document.getElementById("clicker");
 
 var handleTheClick = function(event){
-  // ?????????
+  var thislink = this;
+
+  var req = new XMLHttpRequest();
+
+  req.open("get", thislink.getAttribute("href"));
+
+  req.addEventListener("load", function(){
+    thislink.interText = this.response;
+  });
+
+  req.send();
   
   // Prevent the link from refreshing the DOM.
   event.preventDefault();
