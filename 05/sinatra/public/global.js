930f1ffd7fbbe635d31f4d4f33493623e08e8c73
Kristin Houchin <kmhouchin@gmail.com>
2015-07-13 00:38:50 -0500

    working on 5.

--- 05/sinatra/public/global.js
+++ 05/sinatra/public/global.js
@@ -1,5 +1,19 @@
 var finishLinks = document.getElementsByClassName("done_link");
 
+var mark_task_done = function(event){
+
+var req = new XMLHttpRequest();
+link_with_task = this;
+
+req.open("get", link_with_task.getAttribute("href"));
+
+// finishLinks[i] represents each of the items in `finishLinks`.
+    
+req.send();
+
+event.preventDefault();
+}
+  
 for (var i=0; i < finishLinks.length; i++) {
-  // finishLinks[i] represents each of the items in `finishLinks`.
+  finishLinks[i].onclick = mark_task_done
 }
